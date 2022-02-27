b53b3801d3032d462f55a10c90f0fd05738c3ad9
uriel g <urigalarza@hotmail.com>
2022-02-27 16:56:34 -0500

    added css to sign up component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@
 
 function App() {
   return (
-    <div className="signUp">
-      <h4>Let's <br/><span className="boldedSpan">Sign Up</span> </h4>
-      <p>Use this form to sign up for this super awesome service. You're only a few steps away!</p>
-      <form className="signUpForm">
-        <label>First Name</label>
-        <input></input>
-        <label>Email Address</label>
-        <input></input>
-        <label>Password</label>
-        <input></input>
+    <div className="sign-up-box">
+      <div className="sign-up-content">
+        <p className="heading">Let's <br/><span className="bolded-span">Sign Up</span> </p>
+        <p>Use this form to sign up for this super awesome service. You're only a few steps away!</p>
+        <form className="sign-up-form">
+          <label>First Name</label>
+          <input></input>
+          <label>Email Address</label>
+          <input></input>
+          <label>Password</label>
+          <input></input>
 
-        <button>Sign Up</button>
-      </form>
+          <div className="btn-wrapper">
+            <button>Sign Up</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
