3c6eea323eaac4657a8a489c176098c59186d61c
uriel g <urigalarza@hotmail.com>
2022-02-27 11:26:22 -0500

    defined elements of sign up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import Confirmation from "./components/confirmation";
 
 function App() {
   return (
-    <div>
-      <h1>
-        Sign up
-      </h1>
+    <div className="signUp">
+      <h4>Let's <br/><span className="boldedSpan">Sign Up</span> </h4>
+      <p>Use this form to sign up for this super awesome service. You're only a few steps away!</p>
+      <form className="signUpForm">
+        <label>First Name</label>
+        <input></input>
+        <label>Email Address</label>
+        <input></input>
+        <label>Password</label>
+        <input></input>
+
+        <button>Sign Up</button>
+      </form>
     </div>
   )
 }
---
+++ src/components/confirmation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function confirmation() {
+  return (
+    <div>confirmation</div>
+  )
+}
+
+export default confirmation
