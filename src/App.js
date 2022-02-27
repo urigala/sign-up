1bef86fecc45dfa48f7ae01a6d54e36654855ec4
uriel g <urigalarza@hotmail.com>
2022-02-27 11:57:00 -0500

    restructured files for simplicity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Confirmation from "./components/Confirmation";
-import SignUpForm from "./components/SignUpForm";
+
 
 function App() {
   return (
-    <SignUpForm/>
+    <div className="signUp">
+      <h4>Let's <br/><span className="boldedSpan">Sign Up</span> </h4>
+      <p>Use this form to sign up for this super awesome service. You're only a few steps away!</p>
+      <form className="signUpForm">
+        <label>First Name</label>
+        <input></input>
+        <label>Email Address</label>
+        <input></input>
+        <label>Password</label>
+        <input></input>
+
+        <button>Sign Up</button>
+      </form>
+    </div>
   )
 }
 
