43a4fcea76d5f05eec2643b001e3e2f7ad245483
uriel g <urigalarza@hotmail.com>
2022-02-27 18:23:10 -0500

    added basic form validation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-
+import { useState } from "react";
+import Confirmation from "./Confirmation";
 
 function App() {
+  const [signedUp, setSignedUp] = useState(false)
+
+  if (signedUp) {
+    return (
+      <Confirmation />
+    ) 
+  }
+
   return (
     <div className="sign-up-box">
-      <div className="sign-up-content">
+      <div className="sign-up-content" >
         <p className="heading">Let's <br/><span className="bolded-span">Sign Up</span> </p>
         <p>Use this form to sign up for this super awesome service. You're only a few steps away!</p>
-        <form className="sign-up-form">
+        <form className="sign-up-form" onSubmit={() => setSignedUp(!signedUp)}>
           <label>First Name</label>
-          <input></input>
+          <input required ></input>
           <label>Email Address</label>
-          <input></input>
+          <input required type="email"></input>
           <label>Password</label>
-          <input></input>
-
+          <input required type="password" minLength="6"></input>
           <div className="btn-wrapper">
-            <button>Sign Up</button>
+            <button type="submit" >Sign Up</button>
           </div>
         </form>
       </div>
